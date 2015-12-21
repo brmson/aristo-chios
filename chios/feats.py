2e4b7181c91dc9916932d218ce2ed7a2916ae732
Petr Baudis <pasky@ucw.cz>
2015-12-21 16:29:32 +0100

    Disable GloveFeatures for now, much worse than IR for now, just adds noise

--- chios/feats.py
+++ chios/feats.py
@@ -4,25 +4,28 @@ Ties in the individual specialized feature generators. """
 
 import numpy as np
 
-import chios.feats_glove
+# import chios.feats_glove
 import chios.feats_solr
 import chios.feats_absoccur
 
 
 class FeatureGenerator:
     def __init__(self, glove_dim):
-        self.feat_glove = chios.feats_glove.GloveFeatures(glove_dim)
+        # self.feat_glove = chios.feats_glove.GloveFeatures(glove_dim)
         self.feat_solr = chios.feats_solr.SolrFeatures()
         self.feat_absoccur = chios.feats_absoccur.AbstractCooccurrenceFeatures()
 
     def score(self, q):
         """ generate four-row, n-column feature matrix for the answers """
-        s1 = self.feat_glove.score(q)
-        s2 = self.feat_solr.score(q)
-        s3 = self.feat_absoccur.score(q)
-        s = np.hstack((s1, s2, s3))
+        s = np.hstack((
+            # self.feat_glove.score(q),
+            self.feat_solr.score(q),
+            self.feat_absoccur.score(q)
+        ))
         return s
 
     def labels(self):
         """ output labels for columns in the feature matrix """
-        return self.feat_glove.labels() + self.feat_solr.labels() + self.feat_absoccur.labels()
+        return (
+            # self.feat_glove.labels() +
+            self.feat_solr.labels() + self.feat_absoccur.labels())
