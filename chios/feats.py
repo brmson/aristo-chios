d10671fee81e78fc4fb7e0b017e1de07ab069e73
Petr Baudis <pasky@ucw.cz>
2015-12-29 17:50:43 +0100

    FeatureGenerator: Rewrite to have feature list only at a single place

--- chios/feats.py
+++ chios/feats.py
@@ -11,21 +11,17 @@ import chios.feats_absoccur
 
 class FeatureGenerator:
     def __init__(self, glove_dim):
-        # self.feat_glove = chios.feats_glove.GloveFeatures(glove_dim)
-        self.feat_solr = chios.feats_solr.SolrFeatures()
-        self.feat_absoccur = chios.feats_absoccur.AbstractCooccurrenceFeatures()
+        self.feats = [
+            # chios.feats_glove.GloveFeatures(glove_dim),
+            chios.feats_solr.SolrFeatures(),  # TODO: Configurable enwiki URL
+            chios.feats_absoccur.AbstractCooccurrenceFeatures(),
+        ]
 
     def score(self, q):
         """ generate four-row, n-column feature matrix for the answers """
-        s = np.hstack((
-            # self.feat_glove.score(q),
-            self.feat_solr.score(q),
-            self.feat_absoccur.score(q)
-        ))
+        s = np.hstack([f.score(q) for f in self.feats])
         return s
 
     def labels(self):
         """ output labels for columns in the feature matrix """
-        return (
-            # self.feat_glove.labels() +
-            self.feat_solr.labels() + self.feat_absoccur.labels())
+        return [l for f in self.feats for l in f.labels()]
